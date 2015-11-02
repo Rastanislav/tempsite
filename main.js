f8a7cdc49c9aebe72a36dda3f1982065b162c073
stan <morfij@gmail.com>
2015-11-03 00:20:22 +0100

    jop

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 
 $(window).scroll(function() {
-var navbarHeight = $('.navbar').height(); 
-  var navbarColor = "62,195,246";//color attr for rgba
-  var smallLogoHeight = $('.small-logo').height();
-  var bigLogoHeight = $('.big-logo').height();
+var navbarHeight = $('.header__small-banner').height(); 
+  //var navbarColor = "62,195,246";//color attr for rgba
+  var smallLogoHeight = $('.header__small-banner').height();
   
   
   var smallLogoEndPos = 0;
-  var smallSpeed = (smallLogoHeight / bigLogoHeight);
+  var smallSpeed = (smallLogoHeight / navbarHeight);
   
   var ySmall = ($(window).scrollTop() * smallSpeed); 
   
-  var smallPadding = navbarHeight - ySmall;
-  if (smallPadding > navbarHeight) { smallPadding = navbarHeight; }
+  var smallPadding = (navbarHeight -smallLogoHeight) - ySmall;
+  if (smallPadding > navbarHeight) { smallPadding = navbarHeight;}
   if (smallPadding < smallLogoEndPos) { smallPadding = smallLogoEndPos; }
   if (smallPadding < 0) { smallPadding = 0; }
   
-  $('.small-logo-container ').css({ "padding-top": smallPadding});
+  $('.header__small-banner').css({ "padding-top": smallPadding});
   
   var navOpacity = ySmall / smallLogoHeight; 
   if  (navOpacity > 1) { navOpacity = 1; }
   if (navOpacity < 0 ) { navOpacity = 0; }
-  var navBackColor = 'rgba(' + navbarColor + ',' + navOpacity + ')';
-  $('.navbar').css({"background-color": navBackColor});
+  $('.header__small-banner').css({"opacity": navOpacity});
   
   var shadowOpacity = navOpacity * 0.4;
   if ( ySmall > 1) {
-    $('.navbar').css({"box-shadow": "0 2px 3px rgba(0,0,0," + shadowOpacity + ")"});
+    $('.header__big-banner').css({"box-shadow": "0 2px 3px rgba(0,0,0," + shadowOpacity + ")"});
   } else {
-    $('.navbar').css({"box-shadow": "none"});
+    $('.header__big-banner').css({"box-shadow": "none"});
   }
 });
